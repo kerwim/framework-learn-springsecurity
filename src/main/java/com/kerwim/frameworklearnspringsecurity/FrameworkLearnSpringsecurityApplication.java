aa4196edd0aa5e2a6fdbf532ccf561cdbff66e32
mac <0924>
2024-10-11 23:39:44 +0800

    intro

---
+++ src/main/java/com/kerwim/frameworklearnspringsecurity/FrameworkLearnSpringsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.kerwim.frameworklearnspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkLearnSpringsecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrameworkLearnSpringsecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kerwim/frameworklearnspringsecurity/FrameworkLearnSpringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kerwim.frameworklearnspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworkLearnSpringsecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
